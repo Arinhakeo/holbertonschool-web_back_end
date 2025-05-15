1ac2c46ed5d9824f08ffb185376ffa6b44359d76
Arinhakeo <9295@holbertonstudents.com>
2025-05-15 11:27:34 +0200

    pycodestyle+Readme

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -59,7 +59,7 @@ class Server:
                 data.append(datas[next_index])
                 count += 1
             next_index += 1
-            
+
         return {
             'index': index,
             'data': data,
